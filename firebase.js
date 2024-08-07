703466347a7b27316aeae1727f9946e27a47c103
cookeliam <liamcooke2002@gmail.com>
2024-08-07 01:37:12 -0400

    fixed env vars again

--- firebase.js
+++ firebase.js
@@ -7,7 +7,15 @@ import {getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = process.env.KEYS;
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: "larder-4a098.firebaseapp.com",
+    projectId: "larder-4a098",
+    storageBucket: "larder-4a098.appspot.com",
+    messagingSenderId: process.env.SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
